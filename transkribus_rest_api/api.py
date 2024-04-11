e7524d04a3c286e2ed27b8a5fc1b061d4df2b271
J. Nathanael Philipp <nathanael@philipp.land>
2024-04-11 11:29:43 +0200

    Rearange existing functions and add new ones.

--- transkribus_rest_api/api.py
+++ transkribus_rest_api/api.py
@@ -23,7 +23,9 @@ import requests
 
 from contextlib import contextmanager
 from datetime import datetime, timedelta
-from typing import Dict, List, Final, Generator, Type, TypeVar
+from lxml import etree
+from pathlib import Path
+from typing import Final, Generator, TypeVar
 
 from .types import UploadPage
 from .utils import parse_xml
@@ -57,7 +59,7 @@ class TranskribusRestApi:
             self.session_id = session_id
             self.expires = expires
 
-        def get_auth_header(self) -> Dict[str, str]:
+        def get_auth_header(self) -> dict[str, str]:
             """Get auth token for Authentication header.
 
             Auto refreshes if token is expired.
@@ -100,7 +102,7 @@ class TranskribusRestApi:
             return True
 
         @classmethod
-        def login(cls: Type[T], username: str, password: str) -> T:
+        def login(cls: type[T], username: str, password: str) -> T:
             """Login.
 
             Args:
@@ -123,9 +125,219 @@ class TranskribusRestApi:
                 expires=now + timedelta(hours=12),
             )
 
+    class Collections:
+        """Group all collections requests together."""
+
+        def __init__(self, api: "TranskribusRestApi"):
+            """Create a new collections.
+
+            Args:
+             * api: transkribus rest api
+            """
+            self.api = api
+
+        def get_doc_md_by_id(self, collection_id: int, document_id: int) -> dict:
+            """Get the metadata of a document.
+
+            Args:
+             * collection_id: collection ID
+             * document_id: document ID
+            """
+            return self.api._get(
+                f"collections/{collection_id}/{document_id}/metadata"
+            ).json()
+
+        def get_mets(self, collection_id: int, document_id: int) -> etree._Element:
+            """Get mets file for a document.
+
+            Args:
+             * collection_id: collection ID
+             * document_id: document ID
+            """
+            return parse_xml(
+                self.api._get(f"collections/{collection_id}/{document_id}/mets").content
+            )
+
+        def get_pages_from_pages_str(
+            self,
+            collection_id: int,
+            document_id: int,
+            pages: str | None = None,
+            status: str | None = None,
+            skip_pages_with_missing_status: bool = False,
+        ) -> list:
+            """Get pages in a document.
+
+            Args:
+             * collection_id: collection ID
+             * document_id: document ID
+             * query params: pages, status, skip_pages_with_missing_status
+            """
+            params: dict[str, str | bool] = {
+                "skipPagesWithMissingStatus": skip_pages_with_missing_status
+            }
+            if pages is not None:
+                params["pages"] = pages
+            if status is not None:
+                params["status"] = status
+            return self.api._get(
+                f"collections/{collection_id}/{document_id}/pages", params=params
+            ).json()
+
+        def get_transcript(
+            self, collection_id: int, document_id: int, page_id: int
+        ) -> etree._Element:
+            """Get transcript of a page.
+
+            Args:
+             * collection_id: collection ID
+             * document_id: document ID
+             * page_id: page ID
+            """
+            return parse_xml(
+                self.api._get(
+                    f"collections/{collection_id}/{document_id}/{page_id}/text"
+                ).content
+            )
+
+        def list(
+            self,
+            index: int = 0,
+            n_values: int = 0,
+            sort_column: str | None = None,
+            sort_direction: str | None = None,
+            exclude_empty: bool = False,
+            filter: str | None = None,
+            role: str | None = None,
+            user_id: int | None = None,
+        ) -> dict:
+            """Get a list of collections.
+
+            Args:
+             * index: default 0
+             * nValues: default 0
+             * sort_column
+             * sort_direction
+             * exclude_empty: default False
+            """
+            return self.api._get(
+                "collections",
+                params={
+                    "index": index,
+                    "nValues": n_values,
+                    "sortColumn": sort_column,
+                    "sortDirection": sort_direction,
+                    "excludeEmpty": exclude_empty,
+                    "filter": filter,
+                    "role": role,
+                    "userid": user_id,
+                },
+            ).json()
+
+    class Uploads:
+        """Group all uploads requests together."""
+
+        def __init__(self, api: "TranskribusRestApi"):
+            """Create a new uploads.
+
+            Args:
+             * api: transkribus rest api
+            """
+            self.api = api
+
+        def create_upload_mets(self, collection_id: int, mets: str) -> etree._Element:
+            """Create a new upload from a mets file.
+
+            Args:
+             * collection_id: collection ID
+             * mets: mets XML
+            """
+            return parse_xml(
+                self.api._post(
+                    "uploads",
+                    params={"collId": collection_id},
+                    data={"mets": mets},
+                ).content
+            )
+
+        def create_upload_doc_structure(
+            self, collection_id: int, json: dict
+        ) -> etree._Element:
+            """Create a new upload from JSON data.
+
+            Args:
+             * collection_id: collection ID
+             * json: JSON data
+            """
+            return parse_xml(
+                self.api._post(
+                    "uploads", params={"collId": collection_id}, json=json
+                ).content
+            )
+
+        def upload_page(
+            self,
+            upload_id: int,
+            image: str | Path,
+            xml: str | Path | None = None,
+        ) -> None:
+            """Upload a single page.
+
+            Args:
+             * upload_id: upload ID
+             * image: path to image
+             * xml: optionally path to PAGE-XML
+            """
+            if isinstance(image, str):
+                image = Path(image)
+
+            files = {"img": (image.name, open(image, "rb"), "application/octet-stream")}
+            if xml is not None:
+                if isinstance(xml, str):
+                    xml = Path(xml)
+                files["xml"] = (xml.name, open(xml, "rb"), "application/octet-stream")
+            self.api._put(f"uploads/{upload_id}", files=files)
+
     def __init__(self, username: str, password: str):
         """Init."""
+        self.collections = TranskribusRestApi.Collections(self)
         self.session_id = TranskribusRestApi.SessionId.login(username, password)
+        self.uploads = TranskribusRestApi.Uploads(self)
+
+    def _get(self, path: str, params: dict = {}) -> requests.models.Response:
+        r = requests.get(
+            f"{self.BASE_URL}/{path}",
+            headers=self.session_id.get_auth_header(),
+            params=params,
+        )
+        r.raise_for_status()
+        return r
+
+    def _post(
+        self,
+        path: str,
+        params: dict = {},
+        data: dict = {},
+        json: dict = {},
+    ) -> requests.models.Response:
+        r = requests.get(
+            f"{self.BASE_URL}/{path}",
+            headers=self.session_id.get_auth_header(),
+            params=params,
+            data=data,
+            json=json,
+        )
+        r.raise_for_status()
+        return r
+
+    def _put(self, path: str, files: dict[str, tuple]) -> requests.models.Response:
+        r = requests.put(
+            f"{self.BASE_URL}/{path}",
+            headers=self.session_id.get_auth_header(),
+            files=files,
+        )
+        r.raise_for_status()
+        return r
 
     def close(self) -> bool:
         """Close this API.
@@ -137,57 +349,12 @@ class TranskribusRestApi:
         """
         return self.session_id.logout()
 
-    def get_collections_list(
-        self,
-        index: int = 0,
-        n_values: int = 0,
-        sort_column: str | None = None,
-        sort_direction: str | None = None,
-        empty: bool = False,
-    ) -> Dict:
-        """Get a list of collections.
-
-        Args:
-         * index: default 0
-         * nValues: default 0
-         * sort_column
-         * sort_direction
-         * empty: default False
-        """
-        r = requests.get(
-            f"{self.BASE_URL}/collections/list",
-            headers=self.session_id.get_auth_header(),
-            params={
-                "index": index,
-                "nValues": n_values,
-                "sortColumn": sort_column,
-                "sortDirection": sort_direction,
-                "empty": empty,
-            },
-        )
-        r.raise_for_status()
-        return r.json()
-
-    def get_doc_metadata(self, collection_id: int, document_id: int) -> Dict:
-        """Get the metadata of a document.
-
-        Args:
-         * collection_id: collection ID
-         * document_id: document ID
-        """
-        r = requests.get(
-            f"{self.BASE_URL}/collections/{collection_id}/{document_id}/metadata",
-            headers=self.session_id.get_auth_header(),
-        )
-        r.raise_for_status()
-        return r.json()
-
     def upload_document(
         self,
-        collection_id: str,
+        collection_id: int,
         title: str,
-        pages: List[UploadPage],
-        metadata: Dict = {},
+        pages: list[UploadPage],
+        metadata: dict = {},
     ):
         """Upload a document.
 
@@ -218,35 +385,11 @@ class TranskribusRestApi:
             },
         }
 
-        r = requests.post(
-            f"{self.BASE_URL}/uploads?collId={collection_id}",
-            headers=self.session_id.get_auth_header(),
-            json=data,
-        )
+        doc = self.uploads.create_upload_doc_structure(collection_id, data)
+        upload_id = int(doc.xpath("//uploadId/text()")[0])
 
-        r.raise_for_status()
-        doc = parse_xml(r.content)
-        upload_id = doc.xpath("//uploadId/text()")[0]
         for page in pages:
-            files = {
-                "img": (
-                    page.image.name,
-                    open(page.image, "rb"),
-                    "application/octet-stream",
-                )
-            }
-            if page.page_xml is not None:
-                files["xml"] = (
-                    page.page_xml.name,
-                    open(page.page_xml, "rb"),
-                    "application/octet-stream",
-                )
-            r = requests.put(
-                f"{self.BASE_URL}/uploads/{upload_id}",
-                headers=self.session_id.get_auth_header(),
-                files=files,
-            )
-            r.raise_for_status()
+            self.uploads.upload_page(upload_id, page.image, page.page_xml)
 
 
 @contextmanager
